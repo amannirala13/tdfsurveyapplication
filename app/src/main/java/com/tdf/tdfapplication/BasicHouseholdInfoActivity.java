df814f7b8a4f7cdf246f2e3965774e1973812b9c
sangeetagupta <tinnigupta1998@gmail.com>
2019-08-26 02:46:40 +0530

    fixed exceptions for two screens

--- app/src/main/java/com/tdf/tdfapplication/BasicHouseholdInfoActivity.java
+++ app/src/main/java/com/tdf/tdfapplication/BasicHouseholdInfoActivity.java
@@ -1,14 +1,16 @@
 package com.tdf.tdfapplication;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -16,10 +18,11 @@ public class BasicHouseholdInfoActivity extends AppCompatActivity {
 
     private Spinner spinnerAADHAR;
     private Spinner spinnerSHG;
-    private Spinner spinnerMGNERA, spinnerMaleMGNERA, spinnerFemaleMGNERA;
-    private String countAADHAR,countMGNERA,countMaleMGNERA,countFemaleMGNERA,countSHG;
-    private ArrayList<String> arrayList1,arrayList2,arrayList3,arrayList4,arrayList5;
+    private Spinner spinnerMGNERA, spinnerMaleMGNERA;
+    private String countAADHAR, countMGNERA, countMaleMGNERA, countFemaleMGNERA, countSHG;
+    private ArrayList<String> arrayList1;
     private Button button;
+    private TextView textView;
 
     private int numberOfPeople;
 
@@ -45,39 +48,34 @@ public class BasicHouseholdInfoActivity extends AppCompatActivity {
         spinnerMGNERA.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                countMGNERA =Integer.toString(position);
-                if(position>0) {
-                    spinnerFemaleMGNERA.setVisibility(View.VISIBLE);
+                countMGNERA = Integer.toString(position);
+                if (position > 0) {
+                    textView.setVisibility(View.VISIBLE);
                     spinnerMaleMGNERA.setVisibility(View.VISIBLE);
+                } else {
+                    spinnerMaleMGNERA.setVisibility(View.GONE);
+                    textView.setVisibility(View.GONE);
                 }
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                spinnerFemaleMGNERA.setVisibility(View.GONE);
                 spinnerMaleMGNERA.setVisibility(View.GONE);
+                textView.setVisibility(View.GONE);
             }
         });
-        spinnerFemaleMGNERA.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                countFemaleMGNERA = Integer.toString(position);
-            }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                countFemaleMGNERA = "0";
-            }
-        });
         spinnerMaleMGNERA.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 countMaleMGNERA = Integer.toString(position);
+                countFemaleMGNERA = Integer.toString(arrayList1.size() - position);
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
                 countMaleMGNERA = "0";
+                countFemaleMGNERA = "0";
             }
         });
         spinnerSHG.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -95,44 +93,37 @@ public class BasicHouseholdInfoActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("INFO",PersonalDetailsActivity.PERSON_KEY + "," + countAADHAR + "," + countMGNERA + countMaleMGNERA + countFemaleMGNERA + countSHG);
+                Log.i("INFO", PersonalDetailsActivity.PERSON_KEY + "," + countAADHAR + "," + countMGNERA + countMaleMGNERA + countFemaleMGNERA + countSHG);
                 finish();
             }
         });
     }
 
-    private void initialize(){
+    private void initialize() {
         spinnerAADHAR = findViewById(R.id.respondent_AADHAR_card);
-        spinnerFemaleMGNERA = findViewById(R.id.respondent_MGNERA_card_female);
         spinnerMaleMGNERA = findViewById(R.id.respondent_MGNERA_card_male);
         spinnerMGNERA = findViewById(R.id.respondent_MGNERA_card_count);
         spinnerSHG = findViewById(R.id.respondent_SHG_member);
         button = findViewById(R.id.basic_household_info_submit);
+        textView = findViewById(R.id.respondent_MGNERA_card_male_label);
 
         arrayList1 = new ArrayList<String>();
-        arrayList2 = new ArrayList<String>();
-        arrayList3 = new ArrayList<String>();
-        arrayList4 = new ArrayList<String>();
-        arrayList5 = new ArrayList<String>();
 
-        range(numberOfPeople,arrayList1);
-        range(numberOfPeople,arrayList2);
-        range(numberOfPeople,arrayList3);
-        range(numberOfPeople,arrayList4);
-        range(numberOfPeople,arrayList5);
+        range(numberOfPeople, arrayList1);
 
-        ArrayAdapter<?> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,arrayList1);
+        ArrayAdapter<?> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, arrayList1);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerAADHAR.setAdapter(arrayAdapter);
         spinnerMGNERA.setAdapter(arrayAdapter);
         spinnerSHG.setAdapter(arrayAdapter);
 
         spinnerMaleMGNERA.setAdapter(arrayAdapter);
-        spinnerFemaleMGNERA.setAdapter(arrayAdapter);
+        textView.setVisibility(View.GONE);
+        spinnerMaleMGNERA.setVisibility(View.GONE);
     }
 
-    private void range(int num,ArrayList<String> arr){
-        for(int count =0;count<=num;++count){
+    private void range(int num, ArrayList<String> arr) {
+        for (int count = 0; count <= num; ++count) {
             arr.add(Integer.toString(count));
         }
 
