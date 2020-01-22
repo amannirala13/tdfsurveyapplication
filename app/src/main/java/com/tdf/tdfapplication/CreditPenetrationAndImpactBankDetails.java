1d2d3f23555dc890b816e5979358a8b6d2ee50f1
Lucifer † <35570939+amannirala13@users.noreply.github.com>
2020-01-22 19:35:40 +0530

    Added UI screens

---
+++ app/src/main/java/com/tdf/tdfapplication/CreditPenetrationAndImpactBankDetails.java
@@ -0,0 +1,13 @@
+package com.tdf.tdfapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreditPenetrationAndImpactBankDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit_penetration_and_impact_bank_details);
+    }
+}
---
+++ app/src/main/java/com/tdf/tdfapplication/water_res_dev_ques.java
@@ -0,0 +1,13 @@
+package com.tdf.tdfapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class water_res_dev_ques extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water_res_dev_ques);
+    }
+}
